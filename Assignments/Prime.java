9be01f6facfb72ab5155c105353cc03ae9e58c58
pnaik <pnaik@redhat.com>
2022-02-20 16:00:59 +0530

    The assignment 2 added

--- Assignments/Prime.java
+++ Assignments/Prime.java
@@ -1,23 +1,18 @@
 import java.util.Scanner;
 import java.lang.Math;
 public class Prime {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Enter the number");
-        Scanner sc=new Scanner(System.in);
-        int num=sc.nextInt();
-        int flag=0;
-        for(int i=2;i<=Math.sqrt(num);i++)
-        {
-   if(num%i==0)
-     flag=1;
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int flag = 0;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0)
+                flag = 1;
         }
-
-    
-
-    if(flag==1)
-    System.out.println("the number is not a prime number");
-    else
-    System.out.println("The number is a prime number");
-}
+        if (flag == 1)
+            System.out.println("the number is not a prime number");
+        else
+            System.out.println("The number is a prime number");
+    }
 }
--- Assignments/SelectionSort.java
+++ Assignments/SelectionSort.java
@@ -1,31 +1,24 @@
-import java.util.Scanner;
-import java.lang.Math;
-import java.io.*;
 import java.util.*;
-public class SelectionSort{
-    public static void main(String[] args)
-    {   int a[]={3,4,1,2,8,5};
-    int n=a.length;
-    
-    System.out.println("The array before sorting is"+Arrays.toString(a));
-    selectionSort(a,n);
-System.out.println("The array after Sorting is "+Arrays.toString(a));
+public class SelectionSort {
+    public static void main(String[] args) {
+        int a[] = {3, 4, 1, 2, 8, 5};
+        int n = a.length;
+        System.out.println("The array before sorting is" + Arrays.toString(a));
+        selectionSort(a, n);
+        System.out.println("The array after Sorting is " + Arrays.toString(a));
     }
-
-public static void selectionSort(int a[],int n)
-{int max,temp,i,j;
-  for(i=n-1;i>=0;i--)
-  {  max=i;
-      for(j=0;j<i;j++)
-          if(a[j]>a[max])
-          max=j;
-
-temp=a[max];
-a[max]=a[j];
-a[j]=temp;
-
-      }
-  }
+    public static void selectionSort(int a[], int n) {
+        int max, temp, i, j;
+        for (i = n - 1; i >= 0; i--) {
+            max = i;
+            for (j = 0; j < i; j++)
+                if (a[j] > a[max])
+                    max = j;
+            temp = a[max];
+            a[max] = a[j];
+            a[j] = temp;
+        }
+    }
 
 
 }
